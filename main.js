64397b54dbc694babb1d62293d04ba546b9eb35f
Blu <plush.gamer.456@gmail.com>
2024-01-07 13:06:16 -0500

    ?

--- main.js
+++ main.js
@@ -164,7 +164,9 @@ async function elo_update_text(elo_update, mult, guild, channel, message, player
 
 		if (new_val == 0) {
 			var index = temp_arr.findIndex(val => (val == curr_val))
-			temp_arr = temp_arr.toSpliced(index, 1)
+			if (Array.isArray(temp_arr)) {
+				temp_arr = temp_arr.toSpliced(index, 1)
+			}
 		} else {
 			var index = temp_arr.findIndex(val => (val == curr_val))
 			temp_arr[index] = new_val
