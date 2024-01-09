7cc59a16685ee70e19f820d68f450f50cc617bc6
Blu <plush.gamer.456@gmail.com>
2024-01-09 16:03:54 -0500

    Update history.js

--- commands/history.js
+++ commands/history.js
@@ -45,7 +45,7 @@ async function execute(interaction) {
 
 	var msgs = await to_msgs(Object.values(userDB.data))
 	var content = `# ${displayName}'s History\n${msgs.join("\n")}`
-	content = content.slice(0, 1500)
+	content = content.slice(0, 200)
 
 	await interaction.reply(content)
 }
